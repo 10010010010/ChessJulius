24a360ae087bb53cdb8f96e7204f03f5f4b30e6b
HemdatorJulius <julius-odicho@hotmail.com>
2022-04-29 21:07:00 +0200

    unimportant commit

--- Chess/Form1.cs
+++ Chess/Form1.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
 using System.Windows.Forms;
-using Chess.Properties;
+using PawnGraphics;
 
 namespace Chess
 {
@@ -56,29 +56,20 @@ namespace Chess
             }
         }
 
-        public static void Pawn(object sender, PaintEventArgs e, int x, int y,bool black)
-        {
-            Graphics g = e.Graphics;
-            Pen Black = new Pen(Color.Black, 2);
-            Pen white = new Pen(Color.White, 2);
-            Brush blackBrush = new SolidBrush(Color.Black);
-            Brush whiteBrush = new SolidBrush(Color.White);
-            if (black)
-            {
-                g.FillEllipse(blackBrush, x, y, 50, 50);
-            }
-            else
-            {
-                g.FillEllipse(whiteBrush, x, y, 50, 50);
-            }
-        }
+        
         
         
         
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
             board(sender,e);
+            
           
+        }
+        private void gameStart(object sender, EventArgs e)
+        {
+            gameTimer.Start();
+            
         }
 
         private void gameTimer_Elapsed(object sender, ElapsedEventArgs e)
