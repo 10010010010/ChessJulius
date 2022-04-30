6ff3c719c07a15fbafc0902d1e905da0b7d889ac
0312odju <julius-odicho@hotmail.com>
2022-04-30 16:08:42 +0200

    board is now a bitmap and small changes

--- Chess/movment.cs
+++ Chess/movment.cs
@@ -7,32 +7,13 @@ namespace Chess
     public class movment
     {
      
-        public int[] pieceMovment(object sender,   MouseEventArgs e)
+        public void pieceMovment(object sender,  MouseEventArgs e)
         {
+            //check if left mouse button is down 
             
-           
+         
 
-
-            for (int i = 0; i < 8; i++)
-            {
-                
-                for (int j = 0; j < 8; j++)
-                {
-                    if ((i - 1) * 50 < e.X && e.X < i * 50 && (j - 1) * 50 < e.Y && e.Y < j * 50)
-                    {
-                        int[] arr = new int[2];
-                        arr[0] = i;
-                        arr[1] = j;
-                        return arr;
-                    }
-                    else
-                    {
-                        return null;
-                    }
-                }
-            }
-
-            return null;
+          
         }
     }
 }
--- Chess/pawn.cs
+++ Chess/pawn.cs
@@ -8,6 +8,15 @@ namespace Chess
 {
     class pawnGraphics
     {
+     private static int reach = 1;
+
+        public static int pawnReach
+        {
+            get { return reach;}
+            set { reach = value; }
+
+        }
+        
         public void drawPawn(object sender, PaintEventArgs e, int x, int y, Color color)
         {
             Graphics g = e.Graphics;
